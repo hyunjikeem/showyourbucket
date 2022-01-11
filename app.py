fbf32acd584d8f647e6ed2fb65decc656da2b75a
HyunjiKim818 <kjhxxxx@gmail.com>
2022-01-11 14:05:32 +0900

    주석처리

--- app.py
+++ app.py
@@ -45,11 +45,11 @@ def saving_posts():
 
     return jsonify({'msg':'저장이 완료되었습니다!'})
 
-except jwt.ExpiredSignatureError:
-    return redirect(url_for("login", msg="로그인 시간이 만료되었습니다!"))
-
-except jwt.exceptions.DecodeError:
-    return redirect(url_for("login", msg="로그인 정보가 올바르지 않습니다!"))
+    # except jwt.ExpiredSignatureError:
+    #     return redirect(url_for("login", msg="로그인 시간이 만료되었습니다!"))
+    #
+    # except jwt.exceptions.DecodeError:
+    #     return redirect(url_for("login", msg="로그인 정보가 올바르지 않습니다!"))
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
