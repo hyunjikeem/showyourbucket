171922440a263397e6a927e72bdc1472f8c29ae4
HyunjiKim818 <kjhxxxx@gmail.com>
2022-01-11 12:18:25 +0900

    posting 부분 api

--- app.py
+++ app.py
@@ -1,33 +1,55 @@
 from flask import Flask, render_template, jsonify, request
+
 app = Flask(__name__)
 
 import requests
 from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb://test:test@localhost', 27017)
 # client = MongoClient('localhost', 27017)
 db = client.dbsparta
 
+
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/memo', methods=['GET'])
 def listing():
 
+
     return jsonify({'all_articles': articles})
 
+
 ## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
+@app.route('/api/posts', methods=['POST'])
+def saving_posts():
+    token_receive = request.cookies.get('mytoken') # Client 로 부터 토큰값 가져오기
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256']) #jwt 토큰 받은것을 시크릿키를 가지고 복호화해서 payload에 저장하기
 
+    img = request.form['img']
+    title = request.form['title']
+    desc = request.desc['desc']
 
-    return jsonify({'msg':'기사가 저장되었습니다!'})
+    doc = {
+        'title':title,
+        'img':img,
+        'desc':desc,
+    }
 
-@app.route('/memo/delete', methods=['POST'])
+    db.bucketlist.insert_one(doc)
 
-    return jsonify({'msg': '삭제되었습니다!'})
+    return jsonify({'msg':'저장이 완료되었습니다!'})
+
+except jwt.ExpiredSignatureError:
+    return redirect(url_for("login", msg="로그인 시간이 만료되었습니다!"))
+
+except jwt.exceptions.DecodeError:
+    return redirect(url_for("login", msg="로그인 정보가 올바르지 않습니다!"))
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
